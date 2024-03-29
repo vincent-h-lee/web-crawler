7c372105d14a53474227bf3b72aebdc7cdd1a13a
Vincent Lee <>
2024-03-29 20:40:43 +0000

    http utils

---
+++ api/response.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"vincent-h-lee/web-crawler/util"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	defer func() {
+		if errD := recover(); errD != nil {
+			fmt.Println("Exception occurred at ", util.RecoverExceptionDetails(util.FunctionName()), " and recovered in respondWithJSON function, Error Info: ", errD)
+		}
+	}()
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	defer func() {
+		if errD := recover(); errD != nil {
+			fmt.Println("Exception occurred at ", util.RecoverExceptionDetails(util.FunctionName()), " and recovered in respondWithError function, Error Info: ", errD)
+		}
+	}()
+	respondWithJSON(w, code, map[string]interface{}{"success": false, "errors": map[string]string{"reason": message}})
+}
