295c3b2cbfe3526fd6f2e9b45ce6dd1f28623326
Vincent Lee <>
2024-03-30 15:25:47 +0000

    consumer

--- queue/consumer.go
+++ queue/consumer.go
@@ -2,18 +2,22 @@ package queue
 
 import (
 	"log"
-	"time"
 	"vincent-h-lee/web-crawler/crawler"
 )
 
-// TODO browser pool https://github.com/go-rod/rod/blob/46baf3aad803ed5cd8671aa325cbae4e297a89a4/setup_test.go#L59
-func Consume(u string) {
-	now := time.Now()
-	doc, err := crawler.Crawl(u)
+type consumer struct {
+	repo *crawler.CrawlerRepository
+}
+
+func NewConsumer(repo *crawler.CrawlerRepository) *consumer {
+	return &consumer{repo: repo}
+}
+
+func (c *consumer) Consume(u string) (crawler.CrawlEvent, error) {
+	ev, err := crawler.Crawl(u)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	repo := crawler.CrawlerRepository{}
-	repo.Store(crawler.NewCrawlEvent(u, *doc, now))
+	return c.repo.Store(ev)
 }
