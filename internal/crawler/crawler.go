bff4593ff35916e566659949e75c7368164a6729
Vincent Lee <>
2024-04-03 18:25:08 +0100

    fix: crawler err handling and disable status code

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -8,45 +8,70 @@ import (
 	"time"
 
 	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/proto"
 )
 
 func Crawl(u string, page rod.Page) (CrawlEvent, error) {
 	timestamp := time.Now().UTC()
 	log.Printf("Crawling: %s at %s", u, timestamp)
+	timeoutDuration := 30 * time.Second
 
-	e := proto.NetworkResponseReceived{}
-	wait := page.WaitEvent(&e)
+	/* e := proto.NetworkResponseReceived{}
+	wait := page.Timeout(timeoutDuration).WaitEvent(&e) */
 
 	err := page.Navigate(u)
 	if err != nil {
 		return CrawlEvent{}, errors.Join(errors.New("could not connect to page"), err)
 	}
-	page.MustWaitStable()
-	wait()
 
-	title := page.MustElement("h1").MustText()
+	err = page.Timeout(timeoutDuration).WaitStable(timeoutDuration)
+	//wait()
+
+	if err != nil {
+		return CrawlEvent{}, errors.Join(errors.New("page could not stabilize"), err)
+	}
+
+	titleEl, err := page.Element("h1")
+	if err != nil {
+		return CrawlEvent{}, errors.Join(errors.New("page could not find h1 element"), err)
+	}
+	title, err := titleEl.Text()
+	if err != nil {
+		return CrawlEvent{}, errors.Join(errors.New("page could not find h1 text"), err)
+	}
 
 	var headings []Heading
-	els := page.MustElements("h1,h2,h3,h4,h5,h6")
+	els, err := page.Elements("h1,h2,h3,h4,h5,h6")
+	if err != nil {
+		return CrawlEvent{}, errors.Join(errors.New("page could not find heading elements"), err)
+	}
 	for _, el := range els {
 		headings = append(headings, Heading{Text: strings.TrimSpace(el.MustText()), Tag: el.MustProperty("tagName").String()})
 	}
 
 	var links []Link
 	urlsSet := map[string]bool{}
-	els = page.MustElements("a")
+	els, err = page.Elements("a")
+	if err != nil {
+		return CrawlEvent{}, errors.Join(errors.New("page could not find anchor elements"), err)
+	}
 	for _, el := range els {
-		href := el.MustProperty("href").String()
-		_, err := url.ParseRequestURI(href)
+		property, err := el.Property("href")
+		if err != nil {
+			// just skip if we can't find links
+			continue
+		}
+		href := property.String()
+
+		_, err = url.ParseRequestURI(href)
 
 		if err == nil && !urlsSet[href] {
 			urlsSet[href] = true
-			links = append(links, Link{Url: el.MustProperty("href").String()})
+			links = append(links, Link{Url: href})
 		}
 	}
 
-	ev := NewCrawlEvent(u, title, headings, links, e.Response.Status, timestamp)
+	// TODO store status code
+	ev := NewCrawlEvent(u, title, headings, links, 0, timestamp)
 	log.Printf("Finished crawling %s", u)
 	return ev, nil
 }
